63d2244dea8fc0896c9db5d3dd3bc9b66fb585ed
RomanIlkiv <romalviv93@gmail.com>
2017-04-01 10:34:22 +0200

    Add middledaware for GET user

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var {ObjectID} = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+var { authenticate } = require('./middleware/authenticate');
 
 var app = express();
 var port = process.env.PORT;
@@ -113,18 +114,8 @@ app.post('/users', (req, res) => {
     });
 });
 
-app.get('/users/me', (req, res) => {
-    var token = req.header('x-auth');
-
-    User.findByToken(token).then((user) => {
-        if (!user) {
-            return Promise.reject();
-        }
-
-        res.send(user);
-    }).catch((error) => {
-        res.status(401).send();
-    });
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
